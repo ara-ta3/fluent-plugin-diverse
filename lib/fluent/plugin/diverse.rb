7b79831fb162d3fec548fb66b8534e7dd95e228d
katzchang <katzchang@gmail.com>
2016-05-24 17:06:41 +0900

    init

---
+++ lib/fluent/plugin/diverse.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/diverse/version"
+
+module Fluent
+  module Plugin
+    module Diverse
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent/plugin/diverse/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Diverse
+      VERSION = "0.1.0"
+    end
+  end
+end
